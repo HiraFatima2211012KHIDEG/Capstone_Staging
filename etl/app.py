3a5c03aec3250bf501fa632058180cbe1b2b48a2
Mehmaam2211028KHIDEG <syed.mehmaam@xloopdigital.com>
2023-01-27 22:22:39 +0500

    create docker file and requirement.txt

--- etl/app.py
+++ etl/app.py
@@ -1,25 +1,31 @@
 import logging
-from fastapi import FastAPI, Request
+
 import uvicorn
+from fastapi import FastAPI, Request
+
 logger = logging.getLogger()
 
 app = FastAPI()
+
 
 @app.post("/collect_moisture_mate")
 async def collect_moisture_mate(request: Request):
     moisture_data = await request.json()
     logger.info(moisture_data)
-    return { "message": "ok" }
-    
+    return {"message": "ok"}
+
+
 @app.post("/collect_carbon_sense")
 async def collect_carbon_sense(request: Request):
     carbon_data = await request.json()
     logger.info(carbon_data)
     return {"carbon_data": "ok"}
-   
+
+
 def run_app():
     logging.basicConfig(level=logging.INFO)
     uvicorn.run(app, host="0.0.0.0", port=4008)
+
 
 if __name__ == "__main__":
     run_app()
---
+++ sensors/README.md
@@ -0,0 +1,4 @@
+Run with Docker file 'docker build . -t tag'.
+'docker run tag -p 3000:3000'
+
+we will upgrade it soon.
