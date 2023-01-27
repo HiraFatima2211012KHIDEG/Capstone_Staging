9196c9d399ad2ab77096c8d84c9f747da6fb7ed5
Mehmaam2211028KHIDEG <syed.mehmaam@xloopdigital.com>
2023-01-27 16:17:55 +0500

    create a microservice

---
+++ sensors/etl/app.py
@@ -0,0 +1,31 @@
+import logging
+
+from fastapi import FastAPI, Request
+import uvicorn
+
+
+app = FastAPI()
+
+
+@app.post("/collect_moisture_mate")
+async def collect_moisture_mate(request: Request):
+    moisture_data = await request.json()
+    print(moisture_data)
+    return { "message": "ok" }
+    
+
+@app.post("/collect_carbon_sense")
+async def collect_carbon_sense(request: Request):
+    carbon_data = await request.json()
+    print(carbon_data)
+    return {"carbon_data": "ok"}
+
+   
+
+def run_app():
+    logging.basicConfig(level=logging.INFO)
+    uvicorn.run(app, host="0.0.0.0", port=4008)
+
+
+if __name__ == "__main__":
+    run_app()
