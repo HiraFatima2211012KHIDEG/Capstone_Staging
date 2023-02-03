80759c8cf992749940a11cf44feb4b63eb0f83f3
Mehmaam2211028KHIDEG <syed.mehmaam@xloopdigital.com>
2023-02-03 14:37:56 +0500

    update docker compose file

--- project/extract/extract_moisture_carbon_app.py
+++ project/extract/extract_moisture_carbon_app.py
@@ -1,6 +1,7 @@
 import logging
-import uvicorn
 import os
+
+import uvicorn
 from fastapi import FastAPI, Request
 from kafka import KafkaProducer
 
@@ -8,24 +9,25 @@ app = FastAPI()
 
 logger = logging.getLogger()
 import json
+
 # Kafka settings
 KAFKA_BROKER_URL = os.environ.get("KAFKA_BOOTSTRAP_SERVER")
 Producer = KafkaProducer(
-            bootstrap_servers=KAFKA_BROKER_URL,
-            value_serializer=lambda x: json.dumps(x).encode('utf8'),
-            api_version=(0, 10, 1)
-        )
+    bootstrap_servers=KAFKA_BROKER_URL,
+    value_serializer=lambda x: json.dumps(x).encode("utf8"),
+    api_version=(0, 10, 1),
+)
+
 
 @app.post("/collect_moisture_mate")
 async def collect_moisture_mate(request: Request):
     received_data = await request.json()
     logger.info(f"Received MoistureMate Data: {received_data}")
     try:
-        record = Producer.send('moisturemate', value=received_data)
+        record = Producer.send("moisturemate", value=received_data)
         logger.info(record)
     except:
-        logger.info('didnt run')
-    
+        logger.info("didnt run")
 
 
 @app.post("/collect_carbon_sense")
@@ -33,10 +35,11 @@ async def collect_carbon_sense(request: Request):
     received_data = await request.json()
     logger.info(f"Received CarbonSense Data: {received_data}")
     try:
-        record = Producer.send('carbonsense', value=received_data)
+        record = Producer.send("carbonsense", value=received_data)
         logger.info(record)
     except:
-        logger.info('didnt run')
+        logger.info("didnt run")
+
 
 def run_app():
     logging.basicConfig(level=logging.INFO)
