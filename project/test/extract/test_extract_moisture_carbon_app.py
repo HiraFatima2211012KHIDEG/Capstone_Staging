db2ca36f201b58e008f21d85f5d5dc24dbc1c571
Zubair <zubair@gmail.com>
2023-02-03 12:50:21 +0500

    change post_api testing code

--- project/test/extract/test_extract_moisture_carbon_app.py
+++ project/test/extract/test_extract_moisture_carbon_app.py
@@ -1,30 +1,23 @@
+import requests
 
-from fastapi.testclient import TestClient
-
-from extract.extract_moisture_carbon_app import app
-
-client = TestClient(app)
-
+ENDPOINT = "http://0.0.0.0:4008"
 
 def test_collect_response_for_moisture_mate_sensor():
-    response = client.post(
-        "/collect_moisture_mate",
+    response = requests.post(ENDPOINT + "/collect_moisture_mate",
         json={"test_key": "test_value"},
     )
     assert response.status_code == 200
 
 
 def test_collect_response_for_carbon_sense():
-    response = client.post(
-        "/collect_carbon_sense",
+    response = requests.post(ENDPOINT + "/collect_carbon_sense",
         json={"test_key": "test_value"},
     )
     assert response.status_code == 200
 
 
 def test_server_not_found():
-    response = client.post(
-        "/invalid_endpoint",
+    response = requests.post(ENDPOINT + "/invalid_endpoint",
         json={"test_key": "test_value"},
     )
     assert response.status_code == 404
