8061d54ebe1d536ba50e854ec41c68a0b1daf7c6
zubair <zubair@gmail.com>
2023-02-07 08:32:28 +0500

    test files folder added

--- test/extract/test_extract_moisture_carbon_app.py
+++ test/extract/test_extract_moisture_carbon_app.py
@@ -2,22 +2,26 @@ import requests
 
 ENDPOINT = "http://0.0.0.0:4008"
 
+
 def test_collect_response_for_moisture_mate_sensor():
-    response = requests.post(ENDPOINT + "/collect_moisture_mate",
+    response = requests.post(
+        ENDPOINT + "/collect_moisture_mate",
         json={"test_key": "test_value"},
     )
     assert response.status_code == 200
 
 
 def test_collect_response_for_carbon_sense():
-    response = requests.post(ENDPOINT + "/collect_carbon_sense",
+    response = requests.post(
+        ENDPOINT + "/collect_carbon_sense",
         json={"test_key": "test_value"},
     )
     assert response.status_code == 200
 
 
 def test_server_not_found():
-    response = requests.post(ENDPOINT + "/invalid_endpoint",
+    response = requests.post(
+        ENDPOINT + "/invalid_endpoint",
         json={"test_key": "test_value"},
     )
     assert response.status_code == 404
