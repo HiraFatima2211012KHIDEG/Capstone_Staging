b238afa4ca98688c26cbbbfef5fb5212f798a2d4
Mehmaam2211028KHIDEG <syed.mehmaam@xloopdigital.com>
2023-02-09 17:19:40 +0500

    extract_moisture_carbon_app.py file updated with pre-commit

--- extract/extract_moisture_carbon_app.py
+++ extract/extract_moisture_carbon_app.py
@@ -1,45 +1,47 @@
 import logging
-import uvicorn
 import os
+
+import uvicorn
 from fastapi import FastAPI, Request
 from kafka import KafkaProducer
-from time import sleep
 
 app = FastAPI()
 
-KAFKA_BROKER_URL = os.environ.get('KAFKA_BOOTSTRAP_SERVER')
+KAFKA_BROKER_URL = os.environ.get("KAFKA_BOOTSTRAP_SERVER")
 producer = KafkaProducer(
-            bootstrap_servers=KAFKA_BROKER_URL,
-            value_serializer=lambda x: json.dumps(x).encode('utf8'),
-            api_version=(0, 10, 1)
-        )
+    bootstrap_servers=KAFKA_BROKER_URL,
+    value_serializer=lambda x: json.dumps(x).encode("utf8"),
+    api_version=(0, 10, 1),
+)
 
 logger = logging.getLogger()
 import json
 
-@app.post('/collect_moisture_mate')
+
+@app.post("/collect_moisture_mate")
 async def collect_moisture_mate(request: Request):
     received_data = await request.json()
-    logger.info(f'Received MoistureMate Data: {record}')
+    logger.info(f"Received MoistureMate Data: {record}")
 
     try:
-        record = producer.send('moisturemate', value=received_data)
-        logger.info(f'MoistureMate Data Sent in Kafka: {record}')
-        
-    except:
-        logger.info('Failed to send MoistureMate Data in Kafka')
+        record = producer.send("moisturemate", value=received_data)
+        logger.info(f"MoistureMate Data Sent in Kafka: {record}")
 
-@app.post('/collect_carbon_sense')
+    except:
+        logger.info("Failed to send MoistureMate Data in Kafka")
+
+
+@app.post("/collect_carbon_sense")
 async def collect_carbon_sense(request: Request):
     received_data = await request.json()
-    logger.info(f'Received CarbonSense Data: {record}')
-    
+    logger.info(f"Received CarbonSense Data: {record}")
+
     try:
-        record = producer.send('carbonsense', value=received_data)
-        logger.info(f'CarbonSense Data Sent in Kafka: {record}')
+        record = producer.send("carbonsense", value=received_data)
+        logger.info(f"CarbonSense Data Sent in Kafka: {record}")
     except:
-        logger.info('Failed to send CarbonSense Data in Kafka')
-    
+        logger.info("Failed to send CarbonSense Data in Kafka")
+
 
 def run_app():
     logging.basicConfig(level=logging.INFO)
