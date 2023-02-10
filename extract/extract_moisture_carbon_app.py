35eaa57f1f10638ec259c6fc65454d367e22195a
Grzegorz Jurdzinski <grzegorz.jurdzinski@deepsense.ai>
2023-02-10 09:34:09 +0100

    Fix and add test for extract_moisture_carbon_app.

--- extract/extract_moisture_carbon_app.py
+++ extract/extract_moisture_carbon_app.py
@@ -7,15 +7,11 @@ from fastapi import FastAPI, Request
 from kafka import KafkaProducer
 
 logger = logging.getLogger()
-
 app = FastAPI()
-
-KAFKA_BROKER_URL = os.environ.get("KAFKA_BOOTSTRAP_SERVER")
-producer = KafkaProducer(
-    bootstrap_servers=KAFKA_BROKER_URL,
-    value_serializer=lambda x: json.dumps(x).encode("utf8"),
-    api_version=(0, 10, 1),
-)
+KAFKA_BROKER_URL = os.environ.get("KAFKA_BOOTSTRAP_SERVER", "kafka:29092")
+# producer is set up in `run_app()` function. We do it to avoid setting it up
+# when importing this file as a module, eg. in tests.
+producer = None
 
 
 @app.post("/collect_moisture_mate")
@@ -26,9 +22,8 @@ async def collect_moisture_mate(request: Request):
     try:
         moisture_record = producer.send("moisturemate", value=moisture_data)
         logger.info(f"MoistureMate Data Sent in Kafka: {moisture_record}")
-
-    except:
-        logger.info("Failed to send MoistureMate Data in Kafka")
+    except Exception as err:
+        logger.info(f"Failed to send MoistureMate Data in Kafka: {err}.")
 
 
 @app.post("/collect_carbon_sense")
@@ -39,11 +34,16 @@ async def collect_carbon_sense(request: Request):
     try:
         carbon_record = producer.send("carbonsense", value=carbon_data)
         logger.info(f"CarbonSense Data Sent in Kafka: {carbon_record}")
-    except:
-        logger.info("Failed to send CarbonSense Data in Kafka")
+    except Exception as err:
+        logger.info(f"Failed to send CarbonSense Data in Kafka: {err}.")
 
 
 def run_app():
+    producer = KafkaProducer(
+        bootstrap_servers=KAFKA_BROKER_URL,
+        value_serializer=lambda x: json.dumps(x).encode("utf8"),
+        api_version=(0, 10, 1),
+    )
     logging.basicConfig(level=logging.INFO)
     uvicorn.run(app, host="0.0.0.0", port=4008)
 
