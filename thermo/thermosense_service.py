2f11cb30fbaf9d7c1215062c3d1bb8262a827128
HiraFatima2211012KHIDEG <hira.fatima@xloopdigital.com>
2023-02-13 01:35:55 +0500

    pulled the main level code in functions.

--- thermo/thermosense_service.py
+++ thermo/thermosense_service.py
@@ -11,7 +11,6 @@ import logging
 logger = logging.getLogger()
 
 
-
 # Connect to the Minio S3-compatible bucket
 minio = boto3.client('s3',
                     endpoint_url='http://minio:9000',
@@ -19,14 +18,17 @@ minio = boto3.client('s3',
                     aws_secret_access_key=user_password)
 
 # Get the data from the Minio bucket
-date = datetime.now().replace(second=0, microsecond=0).isoformat()
-data_key = f'smart_thermo/{date}.csv'
+def minio_key():
+    date = datetime.now().replace(second=0, microsecond=0).isoformat()
+    return f'smart_thermo/{date}.csv'
 
 sleep(30)
-data = minio.get_object(Bucket='thermobucketdatabattalion', Key=data_key)
-initial_df = pd.read_csv(data['Body'])
-
-logger.info(f"smart thermo data: {initial_df}")
+def get_data_from_minio():
+    sleep(30)
+    data = minio.get_object(Bucket='thermobucketdatabattalion', Key=minio_key())
+    initial_df = pd.read_csv(data['Body'])
+    logger.info(f"smart thermo data: {initial_df}")
+    return initial_df
  
 KAFKA_BROKER_URL = os.environ.get("KAFKA_BOOTSTRAP_SERVER")
 producer = KafkaProducer(
@@ -35,10 +37,10 @@ producer = KafkaProducer(
             api_version=(0, 10, 1),
             acks=1
         )
-
+def send_data_to_kafka():
 # Send the data to a Kafka topic
-producer.send(topic='thermo', value=initial_df.to_json())
-producer.flush()
-logger.info(f"smart thermo data: {initial_df}")
+    producer.send(topic='thermo', value=get_data_from_minio().to_json())
+    producer.flush()
+    logger.info(f"smart thermo data: {get_data_from_minio()}")
 
 
